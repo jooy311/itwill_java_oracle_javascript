9b9983dcb4218baf96663e9d31f3694b50f82b5e
jooy311 <60565941+jooy311@users.noreply.github.com>
2020-07-06 17:58:49 +0900

    Add files via upload

---
+++ project/ModifyInfoFrame.java
@@ -0,0 +1,273 @@
+package project;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JDesktopPane;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.SQLException;
+import java.util.regex.Pattern;
+
+import javax.swing.JButton;
+import javax.swing.JDesktopPane;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class ModifyInfoFrame extends JFrame implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+
+	private Member member;
+	private ManagerFrame managerFrame;
+	private usermemberDAO dao = usermemberDAO.getDAO();// 이렇게 안하고 싱글톤 패턴을 이용하면 편리함 나중에 바꾸자
+	private JPasswordField passwordField;
+	private JTextField nameTxt;
+	private JTextField myworkplace;
+	private JTextField myPay;
+	private JTextField idTxt;
+	private boolean checkedId = false;
+	private JButton cancelBtn;
+	private JButton changeBtn;
+	private JButton checkIdBtn;
+
+	public boolean flag = false;
+
+	public ModifyInfoFrame(Member member, String title, ManagerFrame managerFrame) {
+		// 아이디 수정 x -> 나머지는 다 허용
+		super(title);
+		this.member = member;
+		this.managerFrame = managerFrame;
+
+		init();
+		start(member);
+
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				dispose();
+			}
+		});
+
+		setBounds(0, 0, 403, 486);
+		setVisible(true);
+		this.setResizable(false);
+
+		// 이벤트 등록
+		cancelBtn.addActionListener(this);
+		changeBtn.addActionListener(this);
+	}
+
+	public void start(Member m) {
+		// 회원아이디를 어디서 어떻게 얻어올것인가..
+		checkedId = true;
+		checkIdBtn.setEnabled(false);
+
+		idTxt.setText(m.getId());
+		passwordField.setText(m.getPwd());
+		nameTxt.setText(m.getName());
+		myworkplace.setText(m.getWorkpalce());
+		myPay.setText(m.getSalperhour());
+
+	}
+
+	// 디자인 만드는 메소드
+	public void init() {
+		getContentPane().setBackground(SystemColor.activeCaption);
+
+		JPanel panel = new JPanel();
+		panel.setForeground(SystemColor.controlLtHighlight);
+		panel.setBackground(new Color(255, 192, 203));
+		getContentPane().add(panel, BorderLayout.NORTH);
+
+		JLabel lblNewLabel = new JLabel("[\uC815\uBCF4 \uC218\uC815]");
+		lblNewLabel.setForeground(new Color(0, 0, 0));
+		lblNewLabel.setFont(new Font("나눔바른펜", Font.PLAIN, 25));
+		panel.add(lblNewLabel);
+
+		JDesktopPane desktopPane = new JDesktopPane();
+		desktopPane.setBackground(new Color(255, 250, 250));
+		getContentPane().add(desktopPane, BorderLayout.CENTER);
+
+		checkIdBtn = new JButton("\uC911\uBCF5\uD655\uC778");
+		checkIdBtn.setEnabled(false);
+		checkIdBtn.setBackground(new Color(255, 182, 193));
+		checkIdBtn.setFont(new Font("나눔고딕코딩", Font.BOLD, 11));
+		checkIdBtn.setBounds(253, 39, 84, 37);
+		desktopPane.add(checkIdBtn);
+
+		JDesktopPane desktopPane_1 = new JDesktopPane();
+		desktopPane_1.setBackground(new Color(255, 250, 250));
+		desktopPane_1.setBounds(153, 336, 224, 60);
+		desktopPane.add(desktopPane_1);
+
+		cancelBtn = new JButton("\uCDE8\uC18C");
+		cancelBtn.setFont(new Font("나눔바른펜", Font.PLAIN, 15));
+		cancelBtn.setBounds(128, 10, 84, 37);
+		desktopPane_1.add(cancelBtn);
+
+		changeBtn = new JButton("\uBCC0\uACBD");
+		changeBtn.setBackground(new Color(255, 182, 193));
+		changeBtn.setFont(new Font("나눔바른펜", Font.PLAIN, 15));
+		changeBtn.setBounds(28, 10, 84, 37);
+		desktopPane_1.add(changeBtn);
+
+		passwordField = new JPasswordField();
+		passwordField.setBounds(143, 99, 204, 35);
+		desktopPane.add(passwordField);
+
+		idTxt = new JTextField();
+		idTxt.setEditable(false);
+		idTxt.setBounds(145, 40, 96, 35);
+		desktopPane.add(idTxt);
+		idTxt.setColumns(10);
+
+		JLabel label = new JLabel(" \uBE44\uBC00\uBC88\uD638 ");
+		label.setFont(new Font("Dialog", Font.PLAIN, 16));
+		label.setBounds(32, 102, 89, 27);
+		desktopPane.add(label);
+
+		JLabel label_1 = new JLabel(" \uC544\uC774\uB514 ");
+		label_1.setFont(new Font("Dialog", Font.PLAIN, 16));
+		label_1.setBounds(44, 45, 58, 27);
+		desktopPane.add(label_1);
+
+		nameTxt = new JTextField();
+		nameTxt.setBounds(143, 163, 155, 35);
+		desktopPane.add(nameTxt);
+		nameTxt.setColumns(10);
+
+		JLabel label_2 = new JLabel("  \uC774\uB984  ");
+		label_2.setBounds(52, 161, 53, 35);
+		desktopPane.add(label_2);
+		label_2.setFont(new Font("Dialog", Font.PLAIN, 15));
+
+		JLabel label_3 = new JLabel(" \uD604\uC7AC \uB098\uC758 \uADFC\uBB34\uC9C0 ");
+		label_3.setBounds(12, 219, 130, 35);
+		desktopPane.add(label_3);
+		label_3.setFont(new Font("Dialog", Font.PLAIN, 13));
+
+		myworkplace = new JTextField();
+		myworkplace.setBounds(143, 221, 233, 35);
+		desktopPane.add(myworkplace);
+		myworkplace.setColumns(10);
+
+		JLabel label_4 = new JLabel(" \uB098\uC758 \uD604\uC7AC \uC2DC\uAE09 ");
+		label_4.setBounds(12, 281, 115, 35);
+		desktopPane.add(label_4);
+		label_4.setFont(new Font("Dialog", Font.PLAIN, 15));
+
+		myPay = new JTextField();
+		myPay.setBounds(143, 283, 233, 35);
+		desktopPane.add(myPay);
+		myPay.setColumns(10);
+	}
+
+	// 이벤트 핸들러
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object src = e.getSource();
+		String pwd = String.valueOf(passwordField.getPassword());
+
+		if (src == cancelBtn) {
+			// setVisible(false);
+			this.dispose();
+		} else if (src == changeBtn) {
+			if (pwd.length() < 4 || pwd.length() > 10) {
+				JOptionPane.showMessageDialog(this, "비밀번호는 4자리 이상 10자리 이하로 설정해주세요!");
+			} else if (nameTxt.getText().length() == 0) {
+				JOptionPane.showMessageDialog(this, "이름을 반드시 입력해주세요!");
+			} else if (myworkplace.getText().length() == 0) {
+				JOptionPane.showMessageDialog(this, "근무지를 반드시 입력해주세요!");
+			} else if (myPay.getText().length() == 0) {
+				JOptionPane.showMessageDialog(this, "시급을 반드시 입력해주세요!");
+			} else if (!Pattern.matches("^\\d+", myPay.getText())) {
+				JOptionPane.showMessageDialog(this, "시급을 정확한 숫자로 입력해주세요!");
+			} else {
+				if (!flag) {
+					if (!myworkplace.getText().equals(member.getWorkpalce())) {
+						int choice = JOptionPane.showConfirmDialog(this, "근무지 변경시, 여태까지 저장된 내용이 초기화됩니다. 그래도 변경하시겠습니까?",
+								"경고", JOptionPane.OK_CANCEL_OPTION);
+						if (choice == JOptionPane.CANCEL_OPTION) {
+							myworkplace.setText(member.getWorkpalce());
+							myworkplace.setEditable(false);
+							flag = false;// 안바꾼다.
+							return;
+						} else if (choice == JOptionPane.OK_OPTION) {
+							flag = true;// 바꾼다.
+							myworkplace.setEditable(false);
+							return;
+						}
+
+					}
+				}
+
+				if (flag) {// 근무지변경으로 초기화할거다
+					member = new Member(idTxt.getText(), String.valueOf(passwordField.getPassword()), nameTxt.getText(),
+							myworkplace.getText(), myPay.getText());
+
+					try {
+						if (dao.update(member)) {
+							JOptionPane.showMessageDialog(this, "변경이 완료되었습니다!");
+
+							// 모든테이블 삭제
+							dao.setinit(member.getId());// 왜 J테이블은 리셋이 안되니?
+							this.dispose();
+							managerFrame.repaint(member);
+
+						} else {
+							JOptionPane.showMessageDialog(this, "변경에 실패하였습니다!!ㅠㅠ");
+						}
+
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				} else {// 근무지 변경안할거다
+					member = new Member(idTxt.getText(), String.valueOf(passwordField.getPassword()), nameTxt.getText(),
+							myworkplace.getText(), myPay.getText());
+
+					try {
+						if (dao.update(member)) {
+
+							JOptionPane.showMessageDialog(this, "변경이 완료되었습니다!");
+							this.dispose();
+							managerFrame.repaint(member);
+
+						} else {
+							JOptionPane.showMessageDialog(this, "변경에 실패하였습니다!!ㅠㅠ");
+						}
+
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				}
+			}
+		}
+
+	}
+}
---
+++ project/usermemberDAO.java
@@ -0,0 +1,360 @@
+package project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+//import project.manager.frame.Member;
+import java.util.Vector;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+//DB와 관련된 등록, 검색, 수정, 삭제 작업을 하는 클래스
+//DAO클래스는 테이블당 1개가 필요하다. 하지만 저는 하나만 작성했습니다...
+//회원정보를 관리하기 위한 DAO클래스이다.
+public class usermemberDAO {
+	private static usermemberDAO instance;
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+//   private DefaultTableModel model;
+
+	public usermemberDAO() {
+		try {
+			// 배포하기위해서는 실제서버주소를 적어주어야 한다.
+			// 1521은 방화벽에 막혀있기 때문에 네트워크로 가서 허용해주도록한다.
+			String ubURL = "jdbc:oracle:thin:@192.168.14.29:1521:xe";
+			String dbID = "scott";
+			String dbPassword = "tiger";
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(ubURL, dbID, dbPassword);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static usermemberDAO getDAO() {
+		usermemberDAO dao = new usermemberDAO();
+		return dao;
+	}
+
+	// 회원정보 불러오기
+	public Member getMember(String id) {
+		Member member = null;
+
+		try {
+			String getmember = "select * from usermember where id = ?";
+			pstmt = conn.prepareStatement(getmember);// sql을 해석해주라
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+
+			if (rs.next())
+				member = new Member(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),
+						rs.getString(5));
+
+		} catch (SQLException e) {
+			System.out.println("getMember 오류" + e.getMessage());
+		}
+
+		db_close();
+
+		return member;
+	}
+
+	// 회원가입
+	public boolean join(Member user) throws SQLException {
+		String SQL = "INSERT INTO USERMEMBER VALUES(? ,? ,? ,?, ?)";
+		try {
+			pstmt = conn.prepareStatement(SQL);// sql을 해석해주라
+
+			pstmt.setString(1, user.getId());
+			pstmt.setString(2, user.getPwd());
+			pstmt.setString(3, user.getName());
+			pstmt.setString(4, user.getWorkpalce());
+			pstmt.setString(5, user.getSalperhour());
+			// return pstmt.executeUpdate();
+			return this.pstmt.executeUpdate() != 0;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		} finally {
+			db_close();
+		}
+
+		return this.pstmt.executeUpdate() != 0;
+	}
+
+	public int checkUser(String id, String pwd) {// 로그인할때 해당 아이디가 맞는지 체크하기 위해
+		int result = -1;
+
+		try {
+			String sql = "SELECT * FROM USERMEMBER WHERE ID = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				if (rs.getString(2).equals(pwd)) {// 맞는경우
+					result = 1;
+				} else {
+					result = 2;// 일치하지 않는경우
+				}
+			} else if (rs.next() == false) {
+				result = 0; // 해당아이디가 없는경우
+			}
+
+		} catch (SQLException e) {
+			System.out.println("로그인 체크 오류" + e.getMessage());
+		}
+
+		// db_close();//사용자가 쓰는 값을 계속 체크해야하기 때문에 여기서 닫지 않는다.
+		return result;
+	}
+
+	// 회원정보 수정
+	public boolean update(Member user) throws SQLException {
+		String SQL = "UPDATE USERMEMBER SET PWD = ?, NAME = ?, WORKPLACE = ?, SALPERHOUR = ? WHERE ID = ?";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, user.getPwd());
+			pstmt.setString(2, user.getName());
+			pstmt.setString(3, user.getWorkpalce());
+			pstmt.setString(4, user.getSalperhour());
+			pstmt.setString(5, user.getId());
+
+			return this.pstmt.executeUpdate() != 0; // true면 수정 성공
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		db_close();
+
+		return this.pstmt.executeUpdate() != 0;
+	}
+
+	public boolean checkId(String id) {
+		boolean isAlreadyUser = false;
+		String checkId = "SELECT ID FROM USERMEMBER WHERE ID = ?";
+		try {
+
+			pstmt = conn.prepareStatement(checkId);
+			pstmt.setString(1, id);
+			if (pstmt.executeQuery().next())
+				isAlreadyUser = false;
+			else
+				isAlreadyUser = true;
+			return !pstmt.executeQuery().next();// isAlreadyUser = true 반환 : 사용가능한 아이디(=검색결과가 없어야한다
+												// pstmt.executeQuery().next()== false)
+												// isAlreadyUser=false반환 : 사용불가 아이디(=검색결과가 있다
+												// pstmt.executeQuery().next()== true)
+
+		} catch (SQLException e) {
+			System.out.println("아이디 중복체크 오류" + e.getMessage());
+		}
+
+		db_close();
+		return isAlreadyUser;
+	}
+
+	// ------------회원당 paymanager관리하는 DAO---------------
+
+	public boolean setinit(String id) throws SQLException {
+		boolean flag = false;
+
+		String sql = "DELETE PAYMANAGER1 WHERE USERID = ?";
+		pstmt = conn.prepareStatement(sql);// sql을 해석해주라
+		pstmt.setString(1, id);
+
+		return this.pstmt.executeUpdate() != 0;// pstmt.executeUpdate() != 0 : 삭제를 한 행이 있다 ? 참 이면 true yy
+	}
+
+	public boolean insertPayData(MemberPayInfo info) throws SQLException {
+		String SQL = "INSERT INTO PAYMANAGER1 VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		try {
+			pstmt = conn.prepareStatement(SQL);// sql을 해석해주라
+
+			pstmt.setString(1, info.getId());
+			pstmt.setString(2, info.getWorkdate());
+			pstmt.setString(3, info.getWorkdateY());
+			pstmt.setString(4, info.getWorkdateM());
+			pstmt.setString(5, info.getWorkdateD());
+			pstmt.setString(6, info.getStarttime());
+			pstmt.setString(7, info.getEndtime());
+			pstmt.setString(8, info.getWorktime());
+			pstmt.setInt(9, info.getDaysal());
+			pstmt.setString(10, info.getSalperhour());
+
+			return this.pstmt.executeUpdate() != 0;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// db_close();
+		return this.pstmt.executeUpdate() != 0;
+	}
+
+	public boolean deleteOnePayData(String id, String workdate) throws SQLException {
+		String SQL = "DELETE FROM PAYMANAGER1 WHERE USERID = ? AND WORKDATE = ?";// 조인안해도 되나..?
+
+		pstmt = conn.prepareStatement(SQL);// sql을 해석해주라
+		pstmt.setString(1, id);// 로그인한 사용자의 id를 매개변수로 받아 그 아이디에 해당하는 정보를 모두 불러오게끔 한다.
+		pstmt.setString(2, workdate);
+
+		return this.pstmt.executeUpdate() != 0;
+	}
+
+	public boolean updateOnePayData(String id, String colname, String worktime, String daysal, String changeValue,
+			String workdateY, String workdateM, String workdateD) throws SQLException {
+
+		if (colname.equals("출근시간")) {
+			colname = "STARTTIME";
+		} else if (colname.equals("퇴근시간")) {
+			colname = "ENDTIME";
+		}
+
+		String SQL = "UPDATE PAYMANAGER1 SET " + colname
+				+ " = ? , WORKTIME = ? , DAYSAL = ? WHERE USERID = ? AND WORKDATEY = ? AND WORKDATEM = ? AND WORKDATED = ? ";
+		// System.out.println(SQL);
+
+		pstmt = conn.prepareStatement(SQL);
+		pstmt.setString(1, changeValue);
+		pstmt.setString(2, worktime);
+		pstmt.setString(3, daysal);
+		pstmt.setString(4, id);
+		pstmt.setString(5, workdateY);
+		pstmt.setString(6, workdateM);
+		pstmt.setString(7, workdateD);
+
+		return this.pstmt.executeUpdate() != 0;
+	}
+
+	public boolean checkWorkDate(String id, String workdate) throws SQLException {// 같은날에 여러개 일을 못하도록 막는다.
+		String sql = "SELECT WORKDATE FROM PAYMANAGER1 WHERE USERID= ? AND WORKDATE = ?";
+		pstmt = conn.prepareStatement(sql);
+
+		pstmt.setString(1, id);
+		pstmt.setString(2, workdate);
+
+		return !pstmt.executeQuery().next();
+	}
+
+	public Vector<Object[]> selectAllData(String id) throws SQLException {
+		// 조인굳이 안해줘도 되는데 그냥 해봤습니다...
+		String SQL = "SELECT WORKDATEY, WORKDATEM, WORKDATED, STARTTIME, ENDTIME, WORKTIME, DAYSAL, SAL_PER_HOUR FROM PAYMANAGER1 JOIN USERMEMBER ON PAYMANAGER1.USERID = USERMEMBER.ID WHERE PAYMANAGER1.USERID = ? ORDER BY WORKDATEY,WORKDATEM,WORKDATED";
+
+		pstmt = conn.prepareStatement(SQL);// sql을 해석해주라
+		pstmt.setString(1, id);// 로그인한 사용자의 id를 매개변수로 받아 그 아이디에 해당하는 정보를 모두 불러오게끔 한다.
+
+		rs = pstmt.executeQuery();
+
+		Vector<Object[]> obj = new Vector<Object[]>();
+
+		while (rs.next()) {// 각각 값을 가져와서 테이블값들을 추가
+			obj.add(new Object[] { rs.getString("WORKDATEY"), rs.getString("WORKDATEM"), rs.getString("WORKDATED"),
+					rs.getString("STARTTIME"), rs.getString("ENDTIME"), rs.getString("WORKTIME"),
+					rs.getString("DAYSAL"), rs.getString("SAL_PER_HOUR") });
+		}
+
+		// db_close();//커밋안해서 계속 안뜨는거였어 ^^...이클립스에서는 auto commit인데 sql에서는 커밋해주어야함!!!!!
+		return obj;
+	}
+
+	public Vector<Object[]> userSelectMonth(String id, String year, String month) throws SQLException {
+		String sql = "SELECT WORKDATEY, WORKDATEM, WORKDATED, STARTTIME, ENDTIME, WORKTIME, DAYSAL, SAL_PER_HOUR FROM PAYMANAGER1 WHERE USERID = ? AND WORKDATEY = ? AND WORKDATEM = ? order by WORKDATEY, WORKDATEM, WORKDATED, STARTTIME, ENDTIME ";
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, id);
+		pstmt.setString(2, year);
+		pstmt.setString(3, month);
+
+		rs = pstmt.executeQuery();
+
+		Vector<Object[]> obj = new Vector<Object[]>();
+
+		if (rs.next()) {
+
+			do {// 각각 값을 가져와서 테이블값들을 추가
+				obj.add(new Object[] { rs.getString("WORKDATEY"), rs.getString("WORKDATEM"), rs.getString("WORKDATED"),
+						rs.getString("STARTTIME"), rs.getString("ENDTIME"), rs.getString("WORKTIME"),
+						rs.getString("DAYSAL"), rs.getString("SAL_PER_HOUR") });
+			} while (rs.next());
+		} else {
+
+		}
+
+		return obj;
+	}
+
+	public String[] calTotalNow(String id, String year, String month) throws SQLException {// 총근무시간, 총번돈
+		String sql = "SELECT WORKTIME, DAYSAL FROM PAYMANAGER1 WHERE USERID = ? AND WORKDATEY = ? AND WORKDATEM = ? order by WORKDATEY, WORKDATEM, WORKDATED, STARTTIME, ENDTIME ";
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, id);
+		pstmt.setString(2, year);
+		pstmt.setString(3, month);
+
+		rs = pstmt.executeQuery();
+
+		int worktimeH = 0, worktimeMM = 0, daySal = 0;
+		String[] str = new String[2];
+
+		while (rs.next()) {
+			if (rs.getString("WORKTIME").length() == 5) {
+				worktimeH += Integer.parseInt(rs.getString("WORKTIME").substring(0, 2));
+				worktimeMM += Integer.parseInt(rs.getString("WORKTIME").substring(3));
+			} else {
+				worktimeH += Integer.parseInt(rs.getString("WORKTIME").substring(0, 1));
+				worktimeMM += Integer.parseInt(rs.getString("WORKTIME").substring(2));
+			}
+
+			daySal += rs.getInt("DAYSAL");
+		}
+
+		int tmpH = worktimeMM / 60;
+		worktimeH += tmpH;
+		worktimeMM = worktimeMM - (60 * tmpH);
+		str[0] = worktimeH + " : " + worktimeMM;
+		str[1] = daySal + "";
+
+		return str;
+	}
+
+	public int calCountworkDay(String id, String year, String month) throws SQLException {// 현재 달의 행을 카운트하면된다. 없으면 0
+		String sql = "SELECT * FROM PAYMANAGER1 WHERE USERID = ? AND WORKDATEY = ? AND WORKDATEM = ?";
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, id);
+		pstmt.setString(2, year);
+		pstmt.setString(3, month);
+		rs = pstmt.executeQuery();
+
+		int count = 0;
+		while (rs.next()) {
+			count++;
+		}
+		return count;
+	}
+
+	public void db_close() {
+
+		try {
+
+			if (pstmt != null)
+				pstmt.close();
+			if (rs != null)
+				rs.close();
+			if (conn != null)
+				conn.close();
+
+		} catch (SQLException e) {
+			System.out.println(e + "=> 닫기 오류");
+		}
+
+	}
+
+}
